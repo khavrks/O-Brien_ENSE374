987ceefdfa7506fdf9b8af7a64021502c8be7795
khavrks <ndk973@uregina.ca>
2022-11-27 17:47:04 -0800

    s

--- django_movies/django_movies/settings.py
+++ django_movies/django_movies/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #pip channels
     # 'channels',
-    "daphne",
+    # "daphne",
 
     "frontend",
     "backend",
@@ -92,15 +92,15 @@ TEMPLATES = [
 ]
 
 
-ASGI_APPLICATION = 'django_movies.asgi.application'
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
+# ASGI_APPLICATION = 'django_movies.asgi.application'
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }
 WSGI_APPLICATION = 'django_movies.wsgi.application'
 
 
